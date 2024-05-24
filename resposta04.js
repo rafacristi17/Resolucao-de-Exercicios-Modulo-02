48f8126cdce09276aee04a3f5dd1019caf27cbae
rafacristi17 <rafaelacristine@live.com>
2024-05-24 00:10:49 -0300

    Resposta da questão 04 concluida

--- resposta04.js
+++ resposta04.js
@@ -12,3 +12,18 @@ let lado02 = parseInt(prompt("Digite o tamanho do lado 02: "));
 let lado03 = parseInt(prompt("Digite o tamanho do lado 03: "));
 
 
+
+if(lado01+lado02>lado03){
+    console.log("Os números dos lados digitados formam um triângulo, pois a soma de dois deles deve ser maior que o comprimento de um");
+}
+else if(lado03+lado01>lado02){
+    console.log("Os números dos lados digitados formam um triângulo, pois a soma de dois deles deve ser maior que o comprimento de um");
+}
+else if(lado02+lado03>lado1){
+    console.log("Os números dos lados digitados formam um triângulo, pois a soma de dois deles deve ser maior que o comprimento de um");
+
+}
+else{
+    console.log("Digite um número válido");
+
+}
