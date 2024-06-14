051a0a995bded1b7d8b9f1d22c2c58ffb15c881c
rafacristi17 <rafaelacristine@live.com>
2024-06-14 17:49:51 -0300

    reposta da questao 12

--- resposta12.js
+++ resposta12.js
@@ -1,2 +1,20 @@
 /*12. Faça um programa que mostre os 10 primeiros elementos da Sequência de Fibonacci.
 Ex.: 1, 1, 2, 3, 5, 8, 13, 21.*/
+
+
+function gerarFibonacci(n) {
+    let fib = [1, 1]; 
+  
+    for (let i = 2; i < n; i++) {
+      fib.push(fib[i - 1] + fib[i - 2]);
+    }
+  
+    return fib;
+  }
+  
+  const numeroDeTermos = 10;
+  
+  const fibonacci = gerarFibonacci(numeroDeTermos);
+  
+  console.log('Os 10 primeiros termos da sequência de Fibonacci são:', fibonacci.join(', '));
+  
