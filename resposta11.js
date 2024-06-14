df64a75186f049267010c3b1655c088874a29a03
rafacristi17 <rafaelacristine@live.com>
2024-06-14 17:36:33 -0300

    resposta da questao 11

--- resposta11.js
+++ resposta11.js
@@ -6,3 +6,25 @@ valores da sequência.
 
 let prompt= require("prompt-sync")();
 
+
+function calcularPA(primeiroTermo, razao) {
+    let termos = [];
+    let soma = 0;
+  
+    for (let i = 0; i < 10; i++) {
+      let termo = primeiroTermo + i * razao;
+      termos.push(termo);
+      soma += termo;
+    }
+  
+    return { termos, soma };
+  }
+  
+  const primeiroTermo = 2;  
+  const razao = 3;  
+
+  let { termos, soma } = calcularPA(primeiroTermo, razao);
+  
+  console.log('Os 10 primeiros termos da PA são:', termos.join(', '));
+  console.log('A soma dos 10 primeiros termos da PA é:', soma);
+  console.log('A Razão da PA é:', razao);
